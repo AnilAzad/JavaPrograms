256c34ca23c871413d470b354c349e6970b8940a
AnilAzad <aniliazad@gmail.com>
2022-08-21 14:16:17 +0530

    QuickSort

---
+++ DSA/Recursion/QuickSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr={8,3,1,4,5,6,2};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr, int low, int high){
+        if (low>=high) {
+            return;
+        }
+        int s=low;
+        int e=high;
+        int mid= s+(e-s)/2;
+        int pivot=arr[mid];
+        while (s <= e) {
+            while (arr[s]<pivot) {
+                s++;
+            }
+            while (arr[e]>pivot) {
+                e--;
+            }
+            if (s<=e) {
+                int temp=arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        sort(arr, low, e);
+        sort(arr, s, high);
+    }
+}
